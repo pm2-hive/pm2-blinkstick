ad71330879adb3c41a269eb2f1a386743f6fb98c
Aymeric Lavit d'Hautefort <alavit-d@student.42.fr>
2016-06-22 15:22:29 +0200

    changed transmission

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var pmx = require('pmx');
 var fs = require('fs');
-var express = require('express');
-var cors = require('cors');
-var app = express();
-var bodyParser = require('body-parser');
+var request = require('request');
 var blinkstick = require('blinkstick');
 var interpolate = require('./interpolate.js');
 
@@ -11,7 +8,7 @@ pmx.initModule({
 
   widget : {
 
-	  logo : 'https://mdn.mozillademos.org/files/3563/HTML5_Logo_128.png',
+    logo : 'https://mdn.mozillademos.org/files/3563/HTML5_Logo_128.png',
 
     theme : ['#262E35', '#1B2228', '#807C7C', '#807C7C'],
 
@@ -32,46 +29,40 @@ pmx.initModule({
 
 }, function(err, conf) {
   //Blinkstick setup
-var led = blinkstick.findFirst();
-led.turnOff();
+  var led = blinkstick.findFirst();
+  led.turnOff();
 
-var blink_val = 1400;
+  var blink_val = 1400;
 
-var current_value = 0;
+  var current_value = 0;
 
-//'#0DFF05';
+  //'#0DFF05';
 
-var opts = {
-  duration : blink_val * 0.8,
-  steps    : 100
-};
+  var opts = {
+    duration : blink_val * 0.8,
+    steps    : 100
+  };
 
-function getColor() {
-  var formula = 140 - (blink_val / 10);
-  console.log(formula);
-  return interpolate(formula);
-}
+  function getColor() {
+    var formula = 140 - (blink_val / 10);
+    return interpolate(formula);
+  }
 
-function rec() {
-  led.pulse(getColor(), opts, function(){
-    setTimeout(rec, blink_val * 0.8);
-  });
-}
+  function rec() {
+    led.pulse(getColor(), opts, function(){
+      setTimeout(rec, blink_val * 0.8);
+    });
+  }
 
-rec();
+  rec();
 
-  //Server setup
-  app.use(cors());
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded ({ extended: true }));
-  app.get('/', function(req, res) {
-    res.send('listening');
-  });
-  app.post('/', function(req, res) {
-    console.log(req.body.status);
-    blink_val = req.body.status;
-    opts.duration = blink_val * 0.8;
-    res.send('received');
-  });
-  app.listen(conf.port);
+  setInterval(function() {
+    request.get("http://" + conf.ip + ":" + conf.port, function(err, res) {
+      if (!err) {
+        console.log(res.body);
+        blink_val = res.body;
+        opts.duration = blink_val * 0.8;
+      }
+    });
+  }, 1000);
 });
